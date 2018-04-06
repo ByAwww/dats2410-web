6f395fbf557d3c5dcb5cd9a8cee7fa4fe390e2b4
byawww <benjamin.aam@gmail.com>
2018-04-06 12:06:58 +0000

    Initial Commit

---
+++ myphp.php
@@ -0,0 +1,1 @@
+<?phpecho"HelloIP:".$_SERVER['REMOTE_ADDR']."</br>";echo"YouareservedbytheserverIP:<b>".$_SERVER['SERVER_ADDR']."</b></br>";$datetime=date("jF,Y,g:ia");echo"on:<b>".$datetime."</b>";?>
